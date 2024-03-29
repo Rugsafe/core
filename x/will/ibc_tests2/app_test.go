33b3e2504cb853af13f49207ef205946e4a55161
0xJovonni <4885245+Jovonni@users.noreply.github.com>
2024-03-29 00:33:47 -0400

    autosave

--- x/will/ibc_tests2/app_test.go
+++ x/will/ibc_tests2/app_test.go
@@ -1,13 +1,32 @@
 package ibctesting
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	"cosmossdk.io/log"
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+
+	corestore "cosmossdk.io/store"
+	storemetrics "cosmossdk.io/store/metrics"
+	storetypes "cosmossdk.io/store/types"
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/runtime"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	capabilitykeeper "github.com/cosmos/ibc-go/modules/capability/keeper"
+	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v8/modules/core/04-channel/types"
+	ibcexported "github.com/cosmos/ibc-go/v8/modules/core/exported"
 	ibctesting "github.com/cosmos/ibc-go/v8/testing"
 
+	//
+	"github.com/CosmWasm/wasmd/app"
+	"github.com/CosmWasm/wasmd/x/will/keeper"
+	"github.com/CosmWasm/wasmd/x/will/types"
+
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -16,6 +35,63 @@ type IBCTestSuite struct {
 	coordinator *ibctesting.Coordinator
 	chainA      *ibctesting.TestChain
 	chainB      *ibctesting.TestChain
+}
+
+func setupKeeper(t *testing.T) (*keeper.Keeper, sdk.Context) {
+	w3llApp := app.Setup(t)
+	mockedCodec := w3llApp.AppCodec()
+
+	// channelKeeper := w3llApp.IBCKeeper.ChannelKeeper
+	// channelKeeper := w3llApp.WillKeeper.ChannelKeeper
+	channelKeeper := w3llApp.GetIBCKeeper().ChannelKeeper
+	// scopedKeeper := w3llApp.ScopedIBCKeeper
+	// scopedKeeper := w3llApp.WillKeeper.ScopedKeeper
+	// scopedKeeper := w3llApp.ScopedKeeper
+
+	fmt.Println(channelKeeper)
+	// fmt.Println(scopedKeeper)
+	// Initialize DB and store
+	memDB := dbm.NewMemDB()
+	// ms := corestore.NewCommitMultiStore(memDB) // Initialize the MultiStore with the in-memory DB
+	ms := corestore.NewCommitMultiStore(memDB, log.NewTestLogger(t), storemetrics.NewNoOpMetrics())
+	// keyWill := storetypes.NewKVStoreKey(types.StoreKey)
+	storeKey := storetypes.NewKVStoreKey(types.StoreKey)
+	ibcStoreKey := storetypes.NewKVStoreKey(ibctransfertypes.StoreKey)    // IBC store key
+	ibcExportedStoreKey := storetypes.NewKVStoreKey(ibcexported.StoreKey) // IBC store key
+	storeservice := runtime.NewKVStoreService(storeKey)
+	memKey := storetypes.NewMemoryStoreKey("mem_capability") // Memory key for capability
+
+	// Create and mount store keys
+	// ms.MountStoreWithDB(keyWill, storetypes.StoreTypeIAVL, memDB)
+	ms.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, memDB)
+	ms.MountStoreWithDB(ibcStoreKey, storetypes.StoreTypeIAVL, memDB)         // Mount the IBC store
+	ms.MountStoreWithDB(ibcExportedStoreKey, storetypes.StoreTypeIAVL, memDB) // Mount the IBC store
+	require.NoError(t, ms.LoadLatestVersion())
+
+	// Create context
+	ctx := sdk.NewContext(ms, tmproto.Header{}, false, log.NewNopLogger())
+
+	// clientKeeper := clientkeeper.NewKeeper(cdc, key, paramSpace, stakingKeeper, upgradeKeeper)
+	// connectionKeeper := connectionkeeper.NewKeeper(cdc, key, paramSpace, clientKeeper)
+	// portKeeper := portkeeper.NewKeeper(scopedKeeper)
+	// channelKeeper := channelkeeper.NewKeeper(cdc, key, clientKeeper, connectionKeeper, &portKeeper, scopedKeeper)
+	capabilityKeeper := capabilitykeeper.NewKeeper(mockedCodec, storeKey, memKey)
+
+	// Initialize keeper with the store key
+	//TODO: FIX
+	// k := keeper.NewKeeper(mockedCodec, storeservice, nil, channelKeeper, w3llApp.GetIBCKeeper().PortKeeper, scopedKeeper, capabilityKeeper)
+	k := keeper.NewKeeper(
+		mockedCodec,
+		storeservice,
+		nil,
+		channelKeeper,
+		w3llApp.GetIBCKeeper().PortKeeper,
+		w3llApp.ScopedWillKeeper,
+		w3llApp.ScopedIBCKeeper,
+		*capabilityKeeper,
+	)
+
+	return &k, ctx
 }
 
 func (suite *IBCTestSuite) SetupTest() {
@@ -34,79 +110,129 @@ func (suite *IBCTestSuite) TestClientCreation() {
 
 }
 
-// func (suite *IBCTestSuite) TestPacketTransmission() {
-// 	// Setup clients, connections, and channels between chainA and chainB
-// 	path := ibctesting.NewPath(suite.chainA, suite.chainB)
-// 	suite.coordinator.Setup(path)
+// util
+// Updates IBC clients without incrementing block height.
+func (suite *IBCTestSuite) updateClientsWithoutBlockIncrement(path *ibctesting.Path) {
+	err := path.EndpointA.UpdateClient()
+	suite.Require().NoError(err)
 
-// 	// Create a packet to send from chainA to chainB
-// 	packet := types.NewPacket([]byte("data"), 1, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, suite.chainA.CurrentHeader.Time.Add(time.Hour))
+	err = path.EndpointB.UpdateClient()
+	suite.Require().NoError(err)
+}
 
-// 	// Send the packet from chainA to chainB
-// 	packetseq, err := path.EndpointA.SendPacket(packet)
-// 	fmt.Println("PacketSequence: %s", packetseq)
-// 	suite.Require().NoError(err)
+// Only increment time and commit blocks without updating clients.
+func (suite *IBCTestSuite) incrementTimeAndCommit() {
+	suite.coordinator.IncrementTimeBy(time.Minute)
+	suite.coordinator.CommitBlock(suite.chainA, suite.chainB)
+}
 
-// 	// Receive the packet on chainB
-// 	err = path.EndpointB.RecvPacket(packet)
-// 	suite.Require().NoError(err)
+func (suite *IBCTestSuite) sync(path *ibctesting.Path) {
+	for path.EndpointB.GetClientState().GetLatestHeight().LT(path.EndpointA.GetClientState().GetLatestHeight()) {
+		suite.incrementTimeAndCommit()
+		suite.updateClientsWithoutBlockIncrement(path)
+	}
+}
 
-// 	// Acknowledge the packet receipt on chainA
-// 	ack := types.NewResultAcknowledgement([]byte{byte(1)})
-// 	err = path.EndpointA.AcknowledgePacket(packet, ack.Acknowledgement())
-// 	suite.Require().NoError(err)
-
-//		// Optionally: Verify that the packet has been received and processed as expected
-//		// This step will depend on the logic of your application
-//	}
 func (suite *IBCTestSuite) TestPacketTransmission() {
-	// Setup clients, connections, and channels between chainA and chainB
 	path := ibctesting.NewPath(suite.chainA, suite.chainB)
 	suite.coordinator.Setup(path)
 
-	// Adjustments for packet creation with height and timeout timestamp
-	currentHeight := suite.chainA.CurrentHeader.Height
-	timeoutHeight := uint64(currentHeight + 100)
-	// Example height timeout
-	// timeoutTimestamp := uint64(time.Now().Add(time.Hour).UnixNano()) // Example timestamp timeout
-	timeoutTimestamp := uint64(time.Now().Add(time.Minute * 10).UnixNano()) // 10 minutes in the future
+	// Adjust timeout height and timestamp
+	futureHeight := uint64(10)
+
+	futureTimestamp := uint64(time.Now().Add(2 * time.Hour).UnixNano())
+
+	timeoutHeight := clienttypes.NewHeight(1, uint64(suite.chainA.CurrentHeader.Height)+futureHeight)
+	timeoutTimestamp := futureTimestamp
 
 	packetData := []byte("data")
 	packet := channeltypes.NewPacket(
 		packetData,
-		1, // sequence, in a real scenario this should be obtained or managed appropriately
+		1,
 		path.EndpointA.ChannelConfig.PortID,
 		path.EndpointA.ChannelID,
 		path.EndpointB.ChannelConfig.PortID,
 		path.EndpointB.ChannelID,
-		clienttypes.NewHeight(0, timeoutHeight), // Using NewHeight with revision number and height
+		timeoutHeight,
 		timeoutTimestamp,
 	)
 
-	// Correcting the send packet process based on expected arguments
-	// Note: The detailed process here might vary depending on your ibc-go version
-	// Assuming you have the correct method to send the packet, we proceed with an example
-	// This step might need to be adjusted to align with your ibc-go version and methods
+	fmt.Printf("Block height before incrementing: A: %d, B: %d\n", suite.chainA.App.LastBlockHeight(), suite.chainB.App.LastBlockHeight())
 
-	// Send the packet from chainA to chainB
-	// This might not directly correspond to your current ibc-go testing setup.
-	// Please adjust based on actual available methods.
-	// The SendPacket operation in your testing suite might need to align with these parameters
-	// Example: err := path.EndpointA.SendPacket(packet)
+	fmt.Println("timeoutHeight: ", timeoutHeight)
+	fmt.Println("timeoutTimestamp: ", timeoutTimestamp)
+	fmt.Println("packet.TimeoutHeight: ", packet.TimeoutHeight)
+	fmt.Println("packet.GetTimeoutTimestamp(): ", packet.GetTimeoutTimestamp())
 
-	// For demonstration, skipping direct packet send operation due to potential method discrepancy
+	fmt.Printf("Block height after first increment: A: %d, B: %d\n", suite.chainA.App.LastBlockHeight(), suite.chainB.App.LastBlockHeight())
 
-	// Receive the packet on chainB
-	err := path.EndpointB.RecvPacket(packet)
-	suite.Require().NoError(err)
+	packetSeq, sendPacketErr := path.EndpointA.SendPacket(packet.TimeoutHeight, packet.GetTimeoutTimestamp(), packetData)
+	fmt.Println("packetSeq: ", packetSeq)
+	suite.Require().NoError(sendPacketErr)
 
-	// Acknowledge the packet receipt on chainA
-	ack := channeltypes.NewResultAcknowledgement([]byte{byte(1)})
-	err = path.EndpointA.AcknowledgePacket(packet, ack.Acknowledgement())
-	suite.Require().NoError(err)
+	//increment?
+	suite.incrementTimeAndCommit()
 
-	// Optionally: Verify that the packet has been received and processed as expected
-	// This step will depend on the logic of your application
+	// Ensure clients on both chains are updated before receiving the packet
+	suite.updateClientsWithoutBlockIncrement(path)
+
+	fmt.Printf("Block height before receiving packet: A: %d, B: %d\n", suite.chainA.App.LastBlockHeight(), suite.chainB.App.LastBlockHeight())
+	recvErr := path.EndpointB.RecvPacket(packet)
+	suite.Require().NoError(recvErr)
+
+	fmt.Printf("Client on ChainA (before ack) at height: %s\n", path.EndpointA.GetClientState().GetLatestHeight())
+	fmt.Printf("Client on ChainB (before ack) at height: %s\n", path.EndpointB.GetClientState().GetLatestHeight())
+
+	// Manually wait for chain B to reach chain A's current height
+	fmt.Println("SANITY CHECK")
+	fmt.Println(path.EndpointB.Chain.CurrentHeader.GetHeight())
+	fmt.Println(path.EndpointA.Chain.CurrentHeader.Height)
+
+	// ack := channeltypes.NewResultAcknowledgement([]byte{byte(1)})
+	ack := channeltypes.NewResultAcknowledgement([]byte{byte(1)}).Acknowledgement()
+	fmt.Println(ack)
+	ackErr := path.EndpointB.AcknowledgePacket(packet, ack)
+	suite.Require().NoError(ackErr)
+
+	fmt.Printf("Final block height: A: %d, B: %d\n", suite.chainA.App.LastBlockHeight(), suite.chainB.App.LastBlockHeight())
+
+}
+
+// func (suite *IBCTestSuite) TestBeginBlockerInteractionWithIBC() {
+//     // Initialize your keeper with mock or real dependencies
+//     myKeeper := keeper.NewKeeper(...)
+
+//     // Setup the state required to trigger the BeginBlocker logic
+//     // For example, create a will that is due for processing
+//     ctx := suite.chainA.GetContext()
+//     myKeeper.CreateWill(ctx, "testWill", ...)
+
+//     // Advance the blockchain to the block height where the will should be processed
+//     suite.coordinator.CommitBlock(suite.chainA, suite.chainB)
+
+//     // Verify that the IBC message was sent as expected
+//     // This might involve checking the mock ChannelKeeper or directly inspecting the state on the receiving chain
+// }
+
+func TestSendIBCMessage(t *testing.T) {
+	// Setup
+	// keeper, ctx := setupKeeper(t)
+	// mockChannelKeeper := MockChannelKeeper{}   // Your mock implementation
+	// keeper.SetChannelKeeper(mockChannelKeeper) // Assuming you have a setter or pass it during initialization
+
+	// // Define your test channel ID, port ID, and data
+	// channelID := "channel-0"
+	// portID := "port-0"
+	// data := []byte("test data")
+
+	// // Act
+	// err := keeper.SendIBCMessage(ctx, channelID, portID, data)
+	// require.NoError(t, err)
+
+	// // Assert
+	// // Verify that SendPacket was called on the mock with expected arguments
+	// mockChannelKeeper.AssertCalled(t, "SendPacket", ctx, channelID, portID, data)
+	// // Further assertions based on your logic
 }
 
 func TestMyIBCTestSuite(t *testing.T) {
