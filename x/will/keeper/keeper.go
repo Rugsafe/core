caac0ad91e917251f976d014df2e2ae0cdb5d2c8
0xJovonni <4885245+Jovonni@users.noreply.github.com>
2024-04-26 02:24:02 -0400

    autosave

--- x/will/keeper/keeper.go
+++ x/will/keeper/keeper.go
@@ -1131,7 +1131,7 @@ func (k *Keeper) hasCapability(ctx sdk.Context, portID string) bool {
 // order to expose it to module's InitGenesis function
 func (k *Keeper) BindPort(ctx sdk.Context, portID string) error {
 	capability := k.portKeeper.BindPort(ctx, portID)
-	fmt.Println("capability")
+	fmt.Println("binding port: capability")
 	fmt.Println(capability)
 	// return k.ClaimCapability(ctx, capability, host.PortPath(portID))
 	return k.scopedKeeper.ClaimCapability(ctx, capability, host.PortPath(portID))
